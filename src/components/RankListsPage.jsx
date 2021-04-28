4c21a68b9e6fff50b2ecc1b469dbb546c0d873a4
Luis Resendiz <80994311+luisrdevy@users.noreply.github.com>
2021-04-27 21:45:15 -0500

    hurones intermedios ranked list

--- src/components/RankListsPage.jsx
+++ src/components/RankListsPage.jsx
@@ -33,9 +33,9 @@ const RankListsPage = () => {
 
 	// const updateOnlyOnce = async () => {
 	// 	await huronesRef.add({
-	// 		username: 'DarKbYte',
-	// 		type: 1,
-	// 		solved: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15, 16, 17, 18, 19, 20 ]
+	// 		username: 'LordTaco',
+	// 		type: 2,
+	// 		solved: []
 	// 	});
 	// };
 
